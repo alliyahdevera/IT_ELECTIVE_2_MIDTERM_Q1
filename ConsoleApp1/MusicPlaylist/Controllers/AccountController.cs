cb58c02ca3360fc7c0cc178bb6a205c48235170e
SolisSophiaCassandra <c1395-24@itmlyceumalabang.onmicrosoft.com>
2026-07-25 08:39:16 +0800

    Solis

---
+++ ConsoleApp1/MusicPlaylist/Controllers/AccountController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicPlaylist.Models;
+
+namespace MusicPlaylist.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var profile = _appData.Profiles.FirstOrDefault(item =>
+                item.Username == model.Username &&
+                item.Password == model.Password);
+
+            if (profile == null)
+            {
+                ModelState.AddModelError(
+                    string.Empty,
+                    "Invalid username or password."
+                );
+
+                return View(model);
+            }
+
+            HttpContext.Session.SetInt32(
+                "ProfileId",
+                profile.ProfileId
+            );
+
+            HttpContext.Session.SetString(
+                "ProfileName",
+                profile.FullName
+            );
+
+            return RedirectToAction(
+                "Index",
+                "Home"
+            );
+        }
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+
+            return RedirectToAction(
+                "Login",
+                "Account"
+            );
+        }
+    }
+}
---
+++ ConsoleApp1/MusicPlaylist/Controllers/ProfileController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicPlaylist.Models;
+
+namespace MusicPlaylist.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly AppData _appData;
+
+        public ProfileController(AppData appData)
+        {
+            _appData = appData;
+        }
+
+        public IActionResult Index()
+        {
+            int? profileId = HttpContext.Session.GetInt32("ProfileId");
+
+            if (profileId == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            var user = _appData.Profiles.FirstOrDefault(u => u.ProfileId == profileId);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.FullName = user.FullName;
+            ViewBag.Username = user.Username;
+            ViewBag.PlaylistCount = user.PlaylistCount;
+
+            return View();
+        }
+    }
+}
